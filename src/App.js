e91735b1488cbc695c79ad4d7d9659babd472a40
amr ashour <amrbahy1996@gmail.com>
2022-03-18 21:22:32 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,65 +1,14 @@
-import React from "react";
-export default class  App extends React.Component {
-   setUpRecaptcha = () => {
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-      "recaptcha-container",
-      {
-        size: "invisible",
-        callback: function (response) {
-          console.log("Captcha Resolved");
-          this.onSignInSubmit();
-        },
-        defaultCountry: "IN",
-      }
-    );
-  };
+import React from 'react';
+import {auth} from './firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import Login from './login';
+import Mainpage from './main';
 
-  onSignInSubmit = (e) => {
-    e.preventDefault();
-    this.setUpRecaptcha();
-    let phoneNumber = "";
-    console.log(phoneNumber);
-    let appVerifier = window.recaptchaVerifier;
-    firebase
-      .auth()
-      .signInWithPhoneNumber(phoneNumber, appVerifier)
-      .then(function (confirmationResult) {
-        window.confirmationResult = confirmationResult;
-        console.log(confirmationResult);
-        console.log("OTP is sent");
-        var code = window.prompt('Enter otp ... !');
-        confirmationResult.confirm(code).then(function (result) {
-          var user = result.user; 
-          console.log("lol success");
-          console.log("ass" + user)
-        }).catch(function (error) {
-          alert('wrong otp'+error)
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-
-
-  render() {
-    return (
-      <div className="container" >
-        <h2 className="">Login</h2>
-        <form className="form" onSubmit={this.onSignInSubmit}>
-          <div id="recaptcha-container"></div>
-
-          <input
-            type="number"
-            name="mobile"
-            placeholder="Mobile Number"
-          />
-
-          <input button="Submit" type="submit" />
-        </form>
-
-      </div>
-    );
-  }
+function App() {
+const [user] = useAuthState(auth);
+return (
+	user ? <Mainpage/> : <Login/>
+);
 }
+
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
 
-  var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-  };
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = { 
+    apiKey : "AIzaSyBw_8FnXlxVWwK9SwPIBSblmS7IGmmg7O0" , 
+    authDomain : "react-login-otp.firebaseapp.com", 
+    projectId : "react-login-otp", 
+    storageBucket : "react-login-otp.appspot.com", 
+    messagingSenderId : "925482459238", 
+    appId : "1:925482459238:web:bdec5b35429214eb5dcda7" 
+};
 
-  export default firebase;
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
+export {auth , firebase};
